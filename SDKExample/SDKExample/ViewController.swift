3585910e4a88b0c4d503498437c6fe27d183e629
mhallin <mhallin@fastmail.com>
2016-09-23 11:50:32 +0200

    Add example project and authentication stub

---
+++ SDKExample/SDKExample/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  SDKExample
+//
+//  Created by Magnus Hallin on 23/09/16.
+//  Copyright © 2016 Allihoopa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
